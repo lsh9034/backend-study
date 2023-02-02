bad4d77e37987526c33b5d9330c066ed817c080c
lsh9034 <lsh9034@kookmin.ac.kr>
2023-02-02 18:35:36 +0900

    3

---
+++ class/13-01-nestjs-with-graphql/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { BoardModule } from './apis/boards/boards.module';
+
+@Module({
+  imports: [
+    BoardModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+  ],
+  //controllers: [AppController],
+  //providers: [AppService],
+})
+export class AppModule {}
