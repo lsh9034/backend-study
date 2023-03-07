c338900dc4110aa84f487b8b328b0f57497cb465
SiHyun Lee <lsh9034@kookmin.ac.kr>
2023-03-07 13:29:07 +0900

    product_entities add

--- class/17-01-typeorm-crud/src/apis/users/entities/user.entity.ts
+++ class/17-01-typeorm-crud/src/apis/users/entities/user.entity.ts
@@ -1,13 +1,18 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {Field, ObjectType} from '@nestjs/graphql';
 
 @Entity()
+@ObjectType()
 export class User {
   @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
   id: string;
 
   @Column()
+  @Field(() => String)
   email: string;
 
   @Column()
+  @Field(() => String)
   password: string;
 }
